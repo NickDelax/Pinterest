d89e19c0496b1dcd3be6e760cbaf0ba8a4eb5cb8
NickDelax <42455587+NickDelax@users.noreply.github.com>
2019-04-11 19:30:30 -0500

    Update PinCell.swift

--- Pinterest/PinCell.swift
+++ Pinterest/PinCell.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+class PinCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    let imageView1 : UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "pinterest")
+        return image
+    }()
+    
+    func setupViews(){
+        self.addSubview(imageView1)
+        
+        imageView1.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        imageView1.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        imageView1.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 9/10).isActive = true
+        imageView1.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 4/5).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:)")
+    }
+}
+
